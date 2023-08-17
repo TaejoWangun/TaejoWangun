3cbab32290a001307501166c2c41d4b1e85ee117
Juni Kang <67307299+woajun@users.noreply.github.com>
2023-08-17 17:28:57 +0900

    #2 Landing 페이지 작업 (#20)
    * add header, footer
    
    * fix header flexibility
    
    * fix footer flexibility
    
    * change global css - width naming
    
    * modify landingpage available mobile

--- frontend/app/(marketing)/components/Footer.tsx
+++ frontend/app/(marketing)/components/Footer.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <p>
-      footer
-    </p>
+    <footer className="flex flex-col items-center justify-center bg-primary min-w-desktop">
+      <Link href="/" className="p-20">
+        <Image src="/images/logo_mark.svg" alt="로고" width={300} height={300} />
+      </Link>
+      <p className="text-zinc-200 pb-12">© 2020 - All rights reserved</p>
+    </footer>
   );
 }
