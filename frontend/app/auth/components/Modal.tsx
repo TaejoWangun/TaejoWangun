7a109ccb6692450631c194549e155117c40d2a0a
ijaesin <ijaesinii@gmail.com>
2023-09-11 20:30:35 +0900

    로그인/회원가입/비밀번호 찾기 페이지 디자인 수정2

--- frontend/app/auth/components/Modal.tsx
+++ frontend/app/auth/components/Modal.tsx
@@ -27,29 +27,33 @@ export default function Modal({ toggleModal, text }: ModalProps) {
     <div className="z-[9999] fixed flex justify-center items-center bg-[rgb(52,64,84,0.7)] top-0 bottom-0 right-0 left-0">
       <div
         ref={modalRef}
-        className="w-64 sm:w-96 rounded-xl border absolute bg-white p-6"
+        className="w-64 sm:w-96 rounded-xl border absolute bg-white p-4 sm:p-6"
       >
-        <div className="flex justify-between items-start mb-4">
+        <div className="flex justify-between items-start mb-2 sm:mb-4">
           <Image
             src="/images/exclamation_mark.svg"
             alt="느낌표 이미지"
-            width={48}
-            height={48}
+            width={32}
+            height={32}
+            className="sm:w-12 sm:h-12"
           />
           <button type="button" onClick={toggleModal}>
             <Image
               src="/images/close.svg"
               alt="닫기 이미지"
-              width={24}
-              height={24}
+              width={18}
+              height={18}
+              className="sm:w-6 sm:h-6"
             />
           </button>
         </div>
-        <h1 className="font-semibold text-lg mb-8">{text}</h1>
+        <h1 className="font-semibold text-base sm:text-lg mb-5 sm:mb-8">
+          {text}
+        </h1>
         <button
           type="button"
           onClick={toggleModal}
-          className="bg-[#FFC000] w-full h-11 rounded-lg"
+          className="bg-[#FFC000] w-full h-8 sm:h-11 rounded-lg"
         >
           확인
         </button>
