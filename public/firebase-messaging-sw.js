e67db8df37bbc65b0b0408551fbea8b179f5869f
woajun <woajun@naver.com>
2023-10-14 20:13:26 +0900

    env static

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-comp
  
 // 이곳에 아까 위에서 앱 등록할때 받은 'firebaseConfig' 값을 넣어주세요.
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD87wnL34Q3Gw_B4h82Adqe_1B07VTrxV0",
+  authDomain: "taejowangun-76ec3.firebaseapp.com",
+  projectId: "taejowangun-76ec3",
+  storageBucket: "taejowangun-76ec3.appspot.com",
+  messagingSenderId: "424634273165",
+  appId: "1:424634273165:web:7e083e53c54ed20b2001c4",
+  measurementId: "G-ZG0CCPCLQ9",
 };
  
 // Initialize Firebase
