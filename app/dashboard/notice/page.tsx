69e9bf81740ff1edfaff7a6d5afd42cc2b79b62f
woajun <woajun@naver.com>
2023-10-16 19:24:31 +0900

    annotation

--- app/dashboard/notice/page.tsx
+++ app/dashboard/notice/page.tsx
@@ -3,30 +3,25 @@
 import { useState } from 'react';
 import { get5PerMin, toWAV } from './kihang';
 
-// // server 요청
-function sendNoti() {
-
-}
-
 export default function Notice() {
   const [ticks, setTicks] = useState<number[]>([]);
 
   async function record() {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    // const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    const mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.ondataavailable = async (event) => {
-      const blob = event.data;
-      const wav = await toWAV(blob);
-      const tick = get5PerMin(wav);
+    // const mediaRecorder = new MediaRecorder(stream);
+    // mediaRecorder.ondataavailable = async (event) => {
+    //   const blob = event.data;
+    //   const pcm = await toPCMwith16(blob);
+    //   const tick = get5PerMin(wav);
 
-      setTicks([...ticks, tick]);
-      const isCheck = tick > 25000;
-      if (isCheck) {
-        sendNoti();
-      }
-    };
-    mediaRecorder.start(1000);
+    //   setTicks([...ticks, tick]);
+    //   const isCheck = tick > 25000;
+    //   if (isCheck) {
+    //     sendNoti();
+    //   }
+    // };
+    // mediaRecorder.start(1000);
   }
   return (
     <p>
