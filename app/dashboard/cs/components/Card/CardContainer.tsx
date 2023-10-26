4f46143336b090f49b957502d16065eeaa96e0da
ijaesin <ijaesinii@gmail.com>
2023-10-26 13:50:38 +0900

    CardContainer css 오류 수정

--- app/dashboard/cs/components/Card/CardContainer.tsx
+++ app/dashboard/cs/components/Card/CardContainer.tsx
@@ -5,7 +5,7 @@ interface CardContainerProps {
 
 export default function CardContainer({ name, children }: CardContainerProps) {
   return (
-    <div className="w-full min-w-[300px] rounded-md overflow-hidden border border-[#006877]">
+    <div className="w-full min-w-[300px] rounded-md border border-[#006877]">
       <div className="bg-[#006877] font-semibold text-[#FFFFFF] pl-2 py-1">
         {name}
       </div>
